b374d1fcbbd83df7352a467612ba35143458a43c
Danila Julev <DanilJulev@gmail.com>
2021-03-09 22:27:06 +0300

    basic js framework setup

---
+++ src/components/excel/Excel.js
@@ -0,0 +1,10 @@
+export class Excel {
+    constructor(selector, options) {
+        this.$el = document.querySelector(selector);
+        this.components = options.components || [];
+    }
+
+    render() {
+        console.log(this.$el);
+    }
+}
