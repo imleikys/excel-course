39012d36607bceb730a0fcff6d5ae461e9257d3c
Danila Julev <daniljulev@gmail.com>
2021-03-16 21:52:20 +0300

    init new branch

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,30 @@ import {createTable} from '@/components/table/table.template';
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
+  constructor($root) {
+    super($root, {
+      name: 'Table',
+      listeners: ['click', 'mousedown', 'mousemove', 'mouseup'],
+    });
+  }
+
   toHTML() {
     return createTable();
+  }
+
+  onClick() {
+    console.log(`click`);
+  }
+
+  onMousedown() {
+    console.log('mousedown');
+  }
+
+  onMousemove() {
+    console.log('mousemove');
+  }
+
+  onMouseup() {
+    console.log('mouseup');
   }
 }
